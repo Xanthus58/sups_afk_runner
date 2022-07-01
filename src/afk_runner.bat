548a8d4499a096fba18ffa4c40b08e2f958e3085
Xanthus <Xanthus58@protonmail.com>
2022-07-02 01:06:54 +0930

    fix: add console logs

--- src/afk_runner.bat
+++ src/afk_runner.bat
@@ -1,1 +1,1 @@
-.\hl2.exe -textmode -64bit -textmode -single_core -nojoy -low -nosound -sw -noshader -nopix -novid -nopreload -nopreloadmodels -multirun  +connect RP.SuperiorServers.co:27015
+.\hl2.exe -textmode -64bit -textmode -single_core -nojoy -low -nosound -sw -noshader -nopix -novid -nopreload -nopreloadmodels -multirun -condebug -conclearlog +connect RP.SuperiorServers.co:27015
